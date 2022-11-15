0748556003c43bca7e1759375b67f3c538de931b
Savitsky Evgeni <zheni11seg@gmail.com>
2022-11-15 16:16:31 +0300

    create project

---
+++ src/main/java/com/example/modsen/ModsenApplication.java
@@ -0,0 +1,13 @@
+package com.example.modsen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModsenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModsenApplication.class, args);
+    }
+
+}
